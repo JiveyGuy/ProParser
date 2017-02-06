3d511579d2475713dd101f9d57b833d65439ebfa
Jason Ivey <Jason Ivey>
2017-02-06 16:57:36 -0700

    Updated main .java file

--- Lab003.java
+++ Lab003.java
@@ -1,15 +1,41 @@
 import java.io.*;
 import java.util.Scanner;
 
+
+/**
+ * This program simply takes input from the coandline arguments
+ *  and parses it to see if it matches any of the keywords in the
+ *  key words.txt file.
+ *
+ * @author  Jason Ivey
+ * @version 1.0
+ * @since   2017-02-06
+ * 
+ */
+
+
 public class Lab003{
   
-  private final boolean DEBUG = true;
+  private static final boolean DEBUG = true; // Boolean var for dev purposes 
+  
+  
+  
+  /**
+   * Main method to accept args as input and call helper methods. 
+   * @param   args[] String arguments from the command line
+   * 
+   */
   
   public static void main(String args[]){
     String[] keywords = {""};
     try {
       keywords = keywordsInput();
+      
       if ( DEBUG ) {
+        for ( String iterator : keywords ) {
+          System.out.print(iterator + " "); 
+        }
+        System.out.println(" are the Keywords. " );
         
       }
     } catch ( KeyWordInputException e ) {
@@ -18,28 +44,57 @@ public class Lab003{
     
     String[] output;
     for ( String iterator : args ) {
+      
       try {
         output = parse(iterator, keywords);
+
         if ( output.length > 1 ) {
           System.out.print("Unable to parse " + iterator + "possible matches are: "); 
           for ( String matches : output ) {
+            
+            if ( DEBUG ) System.out.println("output = " + matches);
+            
             System.out.print(matches + " ");
           }
           System.out.println("");
         } else {
+          
+          if ( DEBUG ) System.out.println("output = " + output[0]);
+          
           System.out.println("Parsed " + iterator + " to match " + output[0]);
         } 
+        
       } catch ( KeyWordInputException e ) {
+        
+        if ( DEBUG ) System.out.println("KeyWordInputException e thrown ");
+        
         continue; 
       }
     }
   }
   
+  
+   /**
+   * Parse method to check for keywords in input
+   * @param     input String input from the command line
+   * @exception KeyWordInputException
+   * @param     keywords[] String[] of the keywords to check for
+   */
+  
   public static String[] parse(String input, String[] keywords) throws KeyWordInputException{
-    boolean hasMatch = false;
-    String processingInput = "";
-    String result[];
-    input = input.toLowerCase();
+    
+    if ( DEBUG ){  //Debug statement
+      System.out.println("parse called with input = " + input + " and keywords " );
+      for ( String iterator : keywords ) {
+        System.out.print(iterator + " " ); 
+      }
+      System.out.println("");
+    }
+    
+    boolean hasMatch = false; //Checks if this input triggered a match
+    String processingInput = ""; //Intermediate string
+    String result[]; //file result
+    input = input.toLowerCase(); 
     
     for ( String keyword : keywords ) {
       if (input.length() != 0 && input.substring( 0 , Math.min(input.length(), keyword.length() ) ).equalsIgnoreCase(keyword.substring( 0 , Math.min(input.length(), keyword.length() ) ) ) ){
@@ -61,9 +116,21 @@ public class Lab003{
     return result;
   }
   
+  
+  /**
+   * Gets input  from keywords.txt file
+   * @exception  KeyWordInputException
+   * @return     returns string[] of keywords from file. 
+   */
+  
   public static String[] keywordsInput() throws KeyWordInputException{
+    if ( DEBUG ) {
+      System.out.println("keywordsInput called");
+    }
+    
     String result[];
     String processingInput = "";
+    
     try{
       Scanner input = new Scanner(new File("keywords.txt"));
       
@@ -82,6 +149,8 @@ public class Lab003{
   
 }
 
+
+// Custom exception specific to this lab
 class KeyWordInputException extends Exception{
   public KeyWordInputException(){
     super();
